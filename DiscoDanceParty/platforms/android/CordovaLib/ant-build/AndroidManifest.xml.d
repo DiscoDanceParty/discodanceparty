37d7f8e503744a474cba8f1e4c53019b125c9d6f
phlickey <pohici@tcd.ie>
2015-04-16 00:12:12 +0100

    Added a function to keep the screen awake when the app is running.

--- DiscoDanceParty/platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
+++ DiscoDanceParty/platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
@@ -1,2 +1,2 @@
-C:\Users\Neil\Documents\Interactive Digital Media\Programming for Digital Media\discodanceparty\DiscoDanceParty\platforms\android\CordovaLib\ant-build\AndroidManifest.xml : \
-C:\Users\Neil\Documents\Interactive Digital Media\Programming for Digital Media\discodanceparty\DiscoDanceParty\platforms\android\CordovaLib\AndroidManifest.xml \
+C:\mobiledevelopment\cs7025\discodanceparty\discodanceparty\DiscoDanceParty\platforms\android\CordovaLib\ant-build\AndroidManifest.xml : \
+C:\mobiledevelopment\cs7025\discodanceparty\discodanceparty\DiscoDanceParty\platforms\android\CordovaLib\AndroidManifest.xml \
--- DiscoDanceParty/platforms/android/ant-build/AndroidManifest.xml.d
+++ DiscoDanceParty/platforms/android/ant-build/AndroidManifest.xml.d
@@ -1,2 +1,2 @@
-C:\Users\Neil\Documents\Interactive Digital Media\Programming for Digital Media\discodanceparty\DiscoDanceParty\platforms\android\ant-build\AndroidManifest.xml : \
-C:\Users\Neil\Documents\Interactive Digital Media\Programming for Digital Media\discodanceparty\DiscoDanceParty\platforms\android\AndroidManifest.xml \
+C:\mobiledevelopment\cs7025\discodanceparty\discodanceparty\DiscoDanceParty\platforms\android\ant-build\AndroidManifest.xml : \
+C:\mobiledevelopment\cs7025\discodanceparty\discodanceparty\DiscoDanceParty\platforms\android\AndroidManifest.xml \
