0cad563ed166a64e30b769d981fd7f2c2ba20bc1
neilocarroll <ocarrone@tcd.ie>
2015-04-15 16:32:34 +0100

    orientation locked to portrait

--- DiscoDanceParty/platforms/android/CordovaLib/ant-gen/R.java.d
+++ DiscoDanceParty/platforms/android/CordovaLib/ant-gen/R.java.d
@@ -1,1 +1,1 @@
- : C:\mobiledevelopment\cs7025\discodanceparty\discodanceparty\DiscoDanceParty\platforms\android\CordovaLib\ant-build\AndroidManifest.xml \
+ : C:\Users\Neil\Documents\Interactive Digital Media\Programming for Digital Media\discodanceparty\DiscoDanceParty\platforms\android\CordovaLib\ant-build\AndroidManifest.xml \
